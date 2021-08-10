1ca30e816cb2a029258caa0d03480987a198ceba
KINGMJ <328047478@qq.com>
2021-08-10 16:57:59 +0800

    使用lazy动态生成路由

--- src/App.js
+++ src/App.js
@@ -1,75 +1,34 @@
 import { Switch, Route } from 'react-router-dom'
-import Demo1 from './pages/demo1'
-import Demo2 from './pages/demo2'
-import Demo3 from './pages/demo3'
-import Demo4 from './pages/demo4'
-import Demo5 from './pages/demo5'
-import Demo6 from './pages/demo6'
-import Demo7 from './pages/demo7'
-import Demo8 from './pages/demo8'
-import Demo9 from './pages/demo9'
-import Demo10 from './pages/demo10'
-import Demo11 from './pages/demo11'
-import Demo12 from './pages/demo12'
+import { lazy, Suspense } from 'react'
 
-const routes = [
-  {
-    path: '/demo1',
-    component: Demo1
-  },
-  {
-    path: '/demo2',
-    component: Demo2
-  },
-  {
-    path: '/demo3',
-    component: Demo3
-  },
-  {
-    path: '/demo4',
-    component: Demo4
-  },
-  {
-    path: '/demo5',
-    component: Demo5
-  },
-  {
-    path: '/demo6',
-    component: Demo6
-  },
-  {
-    path: '/demo7',
-    component: Demo7
-  },
-  {
-    path: '/demo8',
-    component: Demo8
-  },
-  {
-    path: '/demo9',
-    component: Demo9
-  },
-  {
-    path: '/demo10',
-    component: Demo10
-  },
-  {
-    path: '/demo11',
-    component: Demo11
-  },
-  {
-    path: '/demo12',
-    component: Demo12
+const demoCount = 30
+
+/**
+ * 生成demo的路由
+ * @param {*} demoCount
+ */
+const generateDemoRoutes = demoCount => {
+  const demoRoutes = []
+  for (let i = 1; i <= demoCount; i++) {
+    demoRoutes.push({
+      path: `/demo${i}`,
+      component: lazy(() => import(`./pages/demo${i}`))
+    })
   }
-]
+  return demoRoutes
+}
+
+const routes = generateDemoRoutes(demoCount)
 
 function App() {
   return (
-    <Switch>
-      {routes.map((route, index) => (
-        <Route key={index} {...route} />
-      ))}
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route key={index} {...route} />
+        ))}
+      </Switch>
+    </Suspense>
   )
 }
 
---
+++ src/pages/demo13.js
@@ -0,0 +1,5 @@
+const Demo13 = () => {
+  return <div>demo13</div>
+}
+
+export default Demo13
