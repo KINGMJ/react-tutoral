d42ae1bf81a62f9baf34bde8b1914130cdc883ec
KINGMJ <328047478@qq.com>
2021-08-26 16:59:55 +0800

    初始化

--- src/components/demo11/AnotherComponent.js
+++
@@ -1,5 +0,0 @@
-const AnOtherComponent = () => {
-  return <div>另一个其他的组件</div>
-}
-
-export default AnOtherComponent
--- src/components/demo11/OtherComponent.js
+++
@@ -1,5 +0,0 @@
-const OtherComponent = () => {
-  return <div>其他的组件</div>
-}
-
-export default OtherComponent
